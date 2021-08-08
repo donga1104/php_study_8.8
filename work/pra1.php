baab1bbba70f6014cb2c27e9e8ca8f4e9075a248
yoshida <s1y1u1n1@yahoo.co.jp>
2021-08-08 23:40:17 +0900

    オブジェクト編の学習

---
+++ work/pra1.php
@@ -0,0 +1,14 @@
+<?php
+
+$posts = [];
+$posts[0] = ['text' => 'hello', 'likes' => 0];
+$posts[1] = ['text' => 'hello again', 'likes' => 0];
+
+// print_r($posts);
+
+function show($post){
+  printf('%s (%d)' . PHP_EOL, $post['text'], $post['likes']);
+}
+
+show($posts[0]);
+show($posts[1]);
---
+++ work/pra2.php
@@ -0,0 +1,28 @@
+<?php
+// クラス
+class Post{
+
+  // プロパティ
+  public $text;
+  public $likes;
+
+  // メソッド
+  public function show(){
+    printf('%s(%d)' . PHP_EOL, $this->text, $this->likes );
+  }
+}
+
+$posts = [];
+// $posts[0] = ['text' => 'hello', 'likes' => 0];
+$posts[0] = new Post();   // インスタンス
+$posts[0] -> text = 'hello';
+$posts[0] -> likes = 0;
+
+// $posts[1] = ['text' => 'hello again', 'likes' => 0];
+
+$posts[1] = new Post();   // インスタンス
+$posts[1] -> text = 'hello again';
+$posts[1] -> likes = 0;
+
+$posts[0] -> show();
+$posts[1] -> show();
